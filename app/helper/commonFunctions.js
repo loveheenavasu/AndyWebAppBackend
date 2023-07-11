7e42cc2d840d796583e5774810dc7a1c220537a7
loveheenavasu <trivediheenalove@gmail.com>
2023-07-11 10:25:58 +0530

    added the valid status codes

--- app/helper/commonFunctions.js
+++ app/helper/commonFunctions.js
@@ -1,77 +1,106 @@
-const nodeMailer=require('nodemailer');
-const jwt=require('jsonwebtoken');
-const { SMTPMAIL, SMTPPASSWORD, SECRETKEY } = require('../../config');
-const { findOneSession } = require('../services/sessionServices');
+const nodeMailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+const {SMTPMAIL, SMTPPASSWORD, SECRETKEY} = require('../../config');
+const {findOneSession} = require('../services/sessionServices');
 const MESSAGES = require('../utils/messages');
-const helperFunction={};
+const {findOneUser} = require('../services/userServices');
+const helperFunction = {};
 
 /**
  * to send the email
- * @param {*} req 
+ * @param {*} req
  */
-helperFunction.sendEmail=async(req)=>{
-    const transporter=await nodeMailer.createTransport({
-        host:'smtp.gmail.com',
-        port:587,
-        auth:{
-            user:SMTPMAIL,
-            pass:SMTPPASSWORD
-        }
-    });
-    const options={
-        from:SMTPMAIL,
-        to:req.body.email,
-        subject: "verfiy user",
-        text: "please click on the link to login"+req.body.token,
-        html: `<p>hello world <a href="http://localhost:3000/session=${req.body.token}"</a>please click on the given link</p>`,
+helperFunction.sendEmail = async (req) => {
+  const transporter = await nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+      user: SMTPMAIL,
+      pass: SMTPPASSWORD,
+    },
+  });
+  const options = {
+    from: SMTPMAIL,
+    to: req.body.email,
+    subject: 'verfiy user',
+    text: 'please click on the link to login' + req.body.token,
+    html: `<p>hello world <a href='http://localhost:3000/session=${req.body.token}'</a>please click on the given link</p>`,
+  };
+  transporter.sendMail(options, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(info);
     }
-    transporter.sendMail(options,function(error,info){
-        if(error){
-            console.log(error);
-        }
-        else{
-            console.log(info);
-        }
-    })
-}
+  });
+};
 
 /**
  * to generate the token
- * @param {*} payload 
- * @returns 
+ * @param {*} payload
+ * @returns
  */
-helperFunction.generateToken=async(payload)=>{
-    return await jwt.sign(payload,SECRETKEY);
-}
+helperFunction.generateToken = async (payload) => {
+  return await jwt.sign(payload, SECRETKEY);
+};
 
 /**
  * to decrypt the token
- * @param {*} token 
- * @returns 
+ * @param {*} token
+ * @returns
  */
-helperFunction.decryptToken=async(token)=>{
-    return await jwt.verify(token,SECRETKEY);
-}
+helperFunction.decryptToken = async (token) => {
+  return await jwt.verify(token, SECRETKEY);
+};
 
 /**
  * to check the user is admin or not
- * @param {*} req 
- * @param {*} res 
- * @returns 
+ * @param {*} req
+ * @param {*} res
+ * @returns
  */
 
-helperFunction.adminAuthentication=async(req,res)=>{
-    if (!req.headers.authorization) {
-        return res.json({ message: MESSAGES.UNAUTHORIZED_USER });
-      }
-      const token = await findOneSession({
-        token: req.headers.authorization,
-        userType: "admin",
-      });
-      if (!token) {
-        return res.json({ message: MESSAGES.UNAUTHORIZED_USER });
-      }
-      return token;
-}
+helperFunction.adminAuthentication = async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({message: MESSAGES.UNAUTHORIZED_USER});
+  }
+  const token = await findOneSession({
+    token: req.headers.authorization,
+    userType: 'admin',
+  });
+  if (!token) {
+    return res.status(401).json({message: MESSAGES.UNAUTHORIZED_USER});
+  }
+  return token;
+};
 
-module.exports=helperFunction;
+/**
+ * general function to get authentication of the user
+ * @param {*} req
+ * @param {*} res
+ */
+helperFunction.verifyUser = async (req, res) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.status(401).json({message: MESSAGES.UNAUTHORIZED_USER});
+    }
+    const session = await findOneSession({token: req.headers.authorization});
+    if (session) {
+      const decryptToken = await helperFunction.decryptToken(
+          req.headers.authorization,
+      );
+      const user = await findOneUser({_id: decryptToken._id});
+      if (!user) {
+        return res.status(401).json({message: MESSAGES.UNAUTHORIZED_USER});
+      } else {
+        return user;
+      }
+    }
+    return res.status(401).json({message: MESSAGES.UNAUTHORIZED_USER});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({message: 'Internal Server Error'});
+  }
+};
+
+module.exports = helperFunction;
