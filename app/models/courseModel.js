bacbcae2f328bf0a8006e94a1b1fcd1d0d22e0fa
loveheenavasu <trivediheenalove@gmail.com>
2023-07-19 09:55:44 +0530

    changes made related to the questionanswer for unique id

--- app/models/courseModel.js
+++ app/models/courseModel.js
@@ -3,7 +3,7 @@ const mongoose = require( 'mongoose' );
 const questionAnswerSchema = new mongoose.Schema( {
   _id : {
     type : mongoose.Types.ObjectId,
-    default : new mongoose.Types.ObjectId(),
+    auto : true,
     index : true,
   },
   question : {
